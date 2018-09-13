45ad204d17832059daac482fa0eedbe7cd0dc1bb
Hiroya-W <43127622+Hiroya-W@users.noreply.github.com>
2018-09-13 21:49:14 +0900

    end enchant.js

---
+++ img/map/maruta2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="maruta2" tilewidth="16" tileheight="16" tilecount="640" columns="8">
+ <image source="../dlmap/temp/maruta/maruta.png" width="128" height="1280"/>
+</tileset>
--- img/map/retro-city.tsx
+++ img/map/retro-city.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="retro-city" tilewidth="16" tileheight="16" tilecount="480" columns="30">
- <image source="../../../../../Downloads/dlmap/dlmap/retro-city.png" width="480" height="256"/>
+ <image source="../dlmap/retro-city.png" width="480" height="256"/>
 </tileset>
